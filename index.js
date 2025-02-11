6ab413c66737ac371b5425d70a43565adf975425
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2025-02-11 18:09:31 -0500

    Made basic package that makes basic website.

---
+++ index.js
@@ -0,0 +1,104 @@
+const path = require("path");
+const fs = require("fs");
+const readline = require("readline");
+
+const app = readline.createInterface({
+  output: process.stdout,
+  input: process.stdin,
+});
+
+app.question("Name of Project:", function (projectName) {
+  const projectDirectory = path.join("output", projectName);
+  const functionsDirectory = path.join(projectDirectory, "functions");
+  const stylesDirectory = path.join(projectDirectory, "css");
+  const cssFile = path.join(stylesDirectory, "styles.css");
+  const readmeFile = path.join(projectDirectory, "readme.md");
+  const gitIgnoreFile = path.join(projectDirectory, ".gitignore");
+  const indexFile = path.join(projectDirectory, "index.html");
+
+  if (!fs.existsSync(projectDirectory)) {
+    fs.mkdirSync(projectDirectory);
+  }
+
+  if (!fs.existsSync(functionsDirectory)) {
+    fs.mkdirSync(functionsDirectory);
+  }
+
+  if (!fs.existsSync(stylesDirectory)) {
+    fs.mkdirSync(stylesDirectory);
+  }
+
+  fs.writeFileSync(
+    indexFile,
+    `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>${projectName}</title>
+    <link rel="stylesheet" href="css/styles.css" />
+  </head>
+  <body></body>
+</html>`
+  );
+
+  fs.writeFileSync(
+    cssFile,
+    `
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html,
+body {
+  height: 100%;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin-bottom: 16px;
+}
+`
+  );
+
+  fs.writeFileSync(
+    gitIgnoreFile,
+    `
+# Ignore Files and Directories in this folder
+node_modules
+    `
+  );
+
+  fs.writeFileSync(readmeFile, `## Project Name: ${projectName}`);
+
+  app.question("Description of the Project:", function (projectDesc) {
+    fs.appendFileSync(
+      path.join(readmeFile),
+      `
+
+### Project Description:
+
+${projectDesc}`
+    );
+
+    app.question("Author Name:", function (projectAuth) {
+      fs.appendFileSync(
+        path.join(readmeFile),
+        `
+      
+### Project Author:
+
+${projectAuth}`
+      );
+
+      app.close();
+    });
+  });
+});
---
+++ readme.md
@@ -0,0 +1,15 @@
+## Create-Web-Basic
+
+Code that when run, creates basic website for you to work with
+
+### How to Set up
+
+Create Output folder and run index.js.
+
+### Weekly Recap
+
+In this module, I created a function that asks the user a name and then a folder is created inside an output folder. This created folder corresponds with the name given by the user. I implement readline to ask the user in the terminal, path and fs are used to create the folder and direct the path of the folder.
+
+### Link
+
+https://www.npmjs.com/package/web-create-basic
